3764ad391edbc89d16957c2365c79180719c5f02
notharshprajapati <prajapati.harsh0243@gmail.com>
2025-05-21 16:51:07 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-amber-100 underline">
+        Hello world!
+      </h1>
+    </div>
+  );
+};
+
+export default App;
